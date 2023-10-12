b0fb9ff90e6523c46753089b11076283ff98ceaa
gulshan07dev <gulshanku9135@gmail.com>
2023-10-12 12:58:59 +0530

    added listing page

--- src/app/components/listings/ListingCard.tsx
+++ src/app/components/listings/ListingCard.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import useCountries from "@/app/hooks/useCountries";
-import { SafeUser } from "@/app/types";
+import { SafeListing, SafeUser } from "@/app/types";
 import { Listing, Reservation } from "@prisma/client";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
@@ -11,7 +11,7 @@ import Button from "../Button";
 import HeartButton from "../HeartButton";
 
 interface ListingCardProps {
-  data: Listing;
+  data: SafeListing;
   reservation?: Reservation;
   onAction?: (id: string) => void;
   disabled?: boolean;
